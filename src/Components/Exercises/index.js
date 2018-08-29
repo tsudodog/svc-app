41da770d6dfcaa0f165e19f4d9f8c5d54f5e0485
Alex <malamute29@gmail.com>
2018-08-29 01:28:47 -0500

    Episode 4
    ep 4 done

--- src/Components/Exercises/index.js
+++ src/Components/Exercises/index.js
@@ -1,24 +1,62 @@
-import React from "react";
-import { Grid, Paper } from "material-ui";
-import LeftPane from "./LeftPane";
-import RightPane from "./RightPane";
+import React, { Fragment } from "react";
+import { Grid, Paper, Typography, List } from "material-ui";
+import { ListItem, ListItemText } from "material-ui/List";
 
 const styles = {
   Paper: {
     padding: 20,
     marginTop: 10,
-    marginBottom: 10
+    marginBottom: 10,
+    height: 500,
+    overflowY: "auto"
   }
 };
 
-export default props => (
+export default ({
+  exercises,
+  category,
+  onSelect,
+  exercise: {
+    id,
+    title = "Welcome!",
+    description = "Please Select an exercise from the list on the left!"
+  }
+}) => (
   <Grid container>
     <Grid item sm>
-      <LeftPane styles={styles} />
+      <Paper style={styles.Paper}>
+        {exercises.map(
+          ([group, exercises]) =>
+            !category || category === group ? (
+              <Fragment key={group}>
+                <Typography
+                  variant="headline"
+                  style={{ textTransform: "capitalize" }}
+                >
+                  {group}
+                </Typography>
+
+                <List component="ul">
+                  {exercises.map(({ id, title }) => (
+                    <ListItem button key={id} onClick={() => onSelect(id)}>
+                      <ListItemText primary={title} />
+                    </ListItem>
+                  ))}
+                </List>
+              </Fragment>
+            ) : null
+        )}
+      </Paper>
     </Grid>
 
     <Grid item sm>
-      <RightPane styles={styles} />
+      <Paper style={styles.Paper}>
+        <Typography variant="display1">{title}</Typography>
+
+        <Typography variant="display1" />
+        {description}
+        <Typography variant="subheading" />
+      </Paper>
     </Grid>
   </Grid>
 );
