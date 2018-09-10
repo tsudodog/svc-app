94a358729a79b29f12e024c2bffcfc338f9e549e
tsudodog <malamute29@gmail.com>
2018-09-10 08:28:33 -0500

    fursuit card data array example

---
+++ src/FursuitData.js
@@ -0,0 +1,16 @@
+/*
+{
+    name: "Haskell"
+    date: "Jan 15th, 2018"
+    imgSource : ""
+}
+*/
+
+
+[
+    {
+        name:"Haskell",
+        date: "Jan 15th 2018",
+        imgSource : ""
+    }
+]
